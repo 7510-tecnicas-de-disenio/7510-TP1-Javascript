1ac257233ce4d048dbb408329160465589a73107
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-09-30 18:55:17 -0300

    Fact evaluation ok

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,11 +1,29 @@
+const DBLoader = require('../src/dbLoader');
+const Fact = require('../src/fact');
+const Rule = require('../src/rule');
+const _ = require('lodash');
+
 const Interpreter = function () {
+    this.facts = null;
+    this.rules = null;
 
     this.parseDB = function (params, paramss, paramsss) {
-
+        let loader = new DBLoader();
+        loader.loadFromArray(params);
+        this.facts = loader.facts;
+        this.rules = loader.rules;
     };
 
     this.checkQuery = function (params) {
-        return true;
+        if (params.slice(-1) !== ")") return false;
+        let fact = new Fact(params);
+        if (this.facts.some(x => {
+                return _.isEqual(x, fact);
+            })) {
+            return true;
+        } else {
+            let rule = new Rule(params);
+        }
     };
 
 };
--- src/rule.js
+++ src/rule.js
@@ -3,7 +3,9 @@ const Fact = require('../src/fact');
 const Rule = function Rule(string) {
     this.name = string.replace(/\(.*$/, '');
     this.variables = string.replace(/^.*\(/, '').replace(/\).*/, '').split(', ');
-    this.unreplacedFacts = string.replace(/^.*:- /, '').replace(/\), /g, ');').split(';').map((x) => {return new Fact(x)});
+    this.unreplacedFacts = string.replace(/^.*:- /, '').replace(/\), /g, ');').split(';').map((x) => {
+        return new Fact(x)
+    });
 };
 
 Rule.prototype.evaluate = function () {
