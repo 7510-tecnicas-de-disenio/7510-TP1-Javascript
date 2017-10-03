2057fee6c134003a0ab6264802c5a1244e77b91e
Camila de_trincheria <cdetrincheria@hotmail.com>
2017-10-03 03:09:02 -0300

    Methods to obtain predicate, parameters and objectives

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,10 @@
 var Parser = function () {
 
+    var parameterSeparator = ",";
+    var objectiveSeparatorIdentifier = /\),/g;
+    var specialObjectiveSeparator = ";";
+    var specialObjectiveSeparatorIdentifier = ")" + specialObjectiveSeparator;
+
     var queryRegex = /^([a-z]+[a-z_]*)\(([a-z0-9]+)(,[a-z0-9]+)*\)$/;
     var factRegex = /^([a-z]+[a-z_]*)\(([a-z0-9]+)(,[a-z0-9]+)*\)\.$/;
     var ruleRegex = /^([a-z]+[a-z_]*)\(([A-Z]+)(,[A-Z]+)*\):-([a-z]+[a-z-]*)\((\w+)(,\w+)*\)(,([a-z]+[a-z-]*)\((\w+)(,\w+)*\))*\.$/;
@@ -26,6 +31,39 @@ var Parser = function () {
     
     this.validQuery = function(query) {
         return queryRegex.test(query);
+    }
+    
+    this.obtainClausePredicate = function(clause) {
+        var predicateIndex = 0;
+        return clausePredicateRegex.exec(clause)[predicateIndex];
+    }
+    
+    var obtainClauseParameters = function(clause, clauseParametersRegex) {
+        var parametersIndex = 1;
+        var parameterString = clauseParametersRegex.exec(clause)[parametersIndex];
+        return parameterString.split(parameterSeparator);
+    }
+    
+    this.obtainFactParameters = function(fact) {
+        return obtainClauseParameters(fact, factParametersRegex);
+    }
+    
+    this.obtainQueryParameters = function(query) {
+        return obtainClauseParameters(query, queryParametersRegex);
+    }
+    
+    this.obtainRuleVariables = function(rule) {
+        return obtainClauseParameters(rule, ruleVariablesRegex);
+    }
+    
+    var replaceObjectiveSeparator = function(objectives) {
+        return objectives.replace(objectiveSeparatorIdentifier, specialObjectiveSeparatorIdentifier);
+    }
+    
+    this.obtainRuleObjectives = function(rule) {
+        var objectivesIndex = 1;
+        var objectiveString = ruleObjectivesRegex.exec(rule)[objectivesIndex];
+        return replaceObjectiveSeparator(objectiveString).split(specialObjectiveSeparator);
     }
 
 }
